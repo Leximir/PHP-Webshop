d2b9845a2c4ec4f3f0812f79531f24c8c8847454
Leximir <aleksa.stankovic@proton.me>
2025-08-09 18:28:39 +0200

    Basic connection to the database

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
 
 require "functions.php";
-require "router.php";
+// require "router.php";
 
+$dsn="mysql:host=localhost;port=3306;dbname=my_app;charset=utf8mb4";
+$pdo = new PDO($dsn, 'root');
+
+$statement = $pdo->prepare('SELECT * FROM posts');
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post){
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+dd($posts);
