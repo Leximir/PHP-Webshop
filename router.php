a61194db036c4d82c5abd349e532f3feb8d8f49d
Leximir <aleksa.stankovic@proton.me>
2025-08-09 18:05:36 +0200

    Vasic router is built and 404 response view

---
+++ router.php
@@ -0,0 +1,26 @@
+<?php
+
+function routeToController($uri, $routes){
+    if(array_key_exists($uri, $routes)){
+        //dd($routes[$uri]);
+        require $routes[$uri];
+    } else {
+        abort();
+    }
+}
+function abort($code = 404){
+    http_response_code($code);
+    require 'views/404.php';
+    die();
+}
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+$uri = $_SERVER['REQUEST_URI']; // We declare the current uri
+$uri = parse_url($uri); // We parse that uri to path and query. Example: /products?id=5 , becomes array(2) { ["path"]=>string(8) "/products" ["query"]=>string(4) "id=5"}
+$uri = $uri['path']; // We get only the path of the former array
+routeToController($uri, $routes);
