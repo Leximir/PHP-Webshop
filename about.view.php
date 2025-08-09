0dee000aff12f6ec4344e6dd64bb8ca1323b47f9
Leximir <aleksa.stankovic@proton.me>
2025-08-09 13:55:50 +0200

    Added about.php and about.view.php

--- about.view.php
+++ about.view.php
@@ -111,7 +111,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <p>Welcome to the Home page</p>
+            <p>Welcome to the About page</p>
         </div>
     </main>
 </div>
