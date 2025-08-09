aebfbe263a3326f6950f1f549913fa167fddf9c3
Leximir <aleksa.stankovic@proton.me>
2025-08-09 14:21:58 +0200

    Updated views completely with partials

--- views/about.view.php
+++ views/about.view.php
@@ -1,30 +1,10 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>Document</title>
-</head>
-<body class="h-full">
-<div class="min-h-full">
-    <?php require("partials/nav.php") ?>
-
-    <header class="relative bg-white shadow-sm">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold tracking-tight text-gray-900">About</h1>
-        </div>
-    </header>
+<?php require("partials/head.php") ?>
+<?php require("partials/nav.php") ?>
+<?php require("partials/header.php") ?>
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <p>Welcome to the About page</p>
         </div>
     </main>
-</div>
-
-
-</body>
-</html>
+<?php require("partials/footer.php") ?>
