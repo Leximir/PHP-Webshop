48ab06aebe8d2b00dbd4cdd85559139e20d25d20
Leximir <aleksa.stankovic@proton.me>
2025-08-14 17:38:30 +0200

    Added another validation check on creating note paragraph for validating maximum characters in the input which is 1000

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -7,15 +7,20 @@ $heading = "Create Note";
 
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 
+    $body = trim($_POST['body']);
     $errors = [];
 
-    if(strlen(trim($_POST['body'])) === 0){
-        $errors['body'] = "A body is required";
+    if(strlen($body) === 0){
+        $errors['body'] = "A body is required.";
+    }
+
+    if(strlen($body) > 1000){
+        $errors['body'] = "The body cannot be more than 1000 characters.";
     }
 
     if(empty($errors)){
         $db->query("INSERT INTO notes(body, user_id) VALUES(:body, :user_id)", [
-            'body' => $_POST['body'],
+            'body' => $body,
             'user_id' => 3
         ]);
     }
