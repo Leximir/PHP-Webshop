a20a6280a0c0a27c858c4a8794d680f58f765eae
Leximir <aleksa.stankovic@proton.me>
2025-08-14 17:21:40 +0200

    On creatin note controller added validation to check if body input is submited empty

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -16,7 +16,7 @@
                                     <div class="mt-1">
                                     <textarea id="body" name="body" rows="3"
                                               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-                                              placeholder="Here's an idea for a note..."></textarea>
+                                              placeholder="Here's an idea for a note..." required></textarea>
                                     </div>
                                 </div>
                             </div>
