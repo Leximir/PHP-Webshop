4df449193f1237637ff698c852e7be9cade57f47
Leximir <aleksa.stankovic@proton.me>
2025-08-15 18:02:34 +0200

    Added functionality to delete the note

--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -8,6 +8,8 @@ $db = new Database($config['database']);
 
 $heading = "Note";
 
+$currentUserId = 3;
+
 $note = $db->query("SELECT * FROM notes WHERE id = :id", [
     'id' => $_GET['id']
 ])->fetch();
@@ -16,10 +18,21 @@ if (!$note) {
     abort();
 }
 
-$currentUserId = 3;
-
 if ($note['user_id'] !== $currentUserId) {
     abort(Response::FORBIDDEN);
+}
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){
+    // Delete the current note
+
+    $db->query('DELETE FROM notes WHERE id = :id AND user_id = :user_id',[
+        'id' => $_POST['id'],
+        'user_id' => $currentUserId
+    ]);
+
+    header("Location: /notes");
+    exit();
+
 }
 
 view("notes/show.view.php",[
--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -7,9 +7,16 @@
             <p class="mb-6">
                 <a href="/notes" class="text-blue-500 hover:underline">go back ...</a>
             </p>
+
             <p>
                 <?= htmlspecialchars($note['body']) ?>
             </p>
+
+            <form class="mt-6" method="POST">
+                <input type="hidden" name="id" value="<?= $note['id'] ?>">
+                <button class="text-sm text-red-500" type="submit">Delete</button>
+            </form>
+
         </div>
     </main>
 
