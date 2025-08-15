be8b089a3be233cadb70e2d999286c48c44c85fd
Leximir <aleksa.stankovic@proton.me>
2025-08-15 17:40:28 +0200

    Added namespaces to class files and tweaked them to work in controllers

--- Core/Validator.php
+++ Core/Validator.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Core;
 class Validator
 {
     public static function stringCheck($value, $min = 1, $max = 1000){
--- public/index.php
+++ public/index.php
@@ -5,7 +5,10 @@ const BASE_PATH = __DIR__ . '/../';
 require BASE_PATH . "Core/functions.php";
 
 spl_autoload_register(function ($class){
-    require base_path("Core/{$class}.php");
+//  We added namespaces to a lot of classes and now we need to tweak this function to work properly
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class); // This: Core\Database becomses : Core/Database, and continues to work properly
+    
+    require base_path("{$class}.php");
 });
 
 require base_path("Core/router.php");
