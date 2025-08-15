b4d7a91ec2e45b843dc6ff599819c4d15006c25a
Leximir <aleksa.stankovic@proton.me>
2025-08-15 17:29:35 +0200

    Moved functions.php and router.php to Core directory

--- Core/router.php
+++ Core/router.php
@@ -3,8 +3,9 @@
 function routeToController($uri, $routes)
 {
     if (array_key_exists($uri, $routes)) {
-        //dd($routes[$uri]);
-        require $routes[$uri];
+
+        require base_path($routes[$uri]);
+
     } else {
         abort();
     }
@@ -13,11 +14,11 @@ function routeToController($uri, $routes)
 function abort($code = 404)
 {
     http_response_code($code);
-    require 'views/' . $code . '.php';
+    require base_path('views/' . $code . '.php');
     die();
 }
 
-$routes = require 'routes.php';
+$routes = require base_path('routes.php');
 
 $uri = $_SERVER['REQUEST_URI']; // We declare the current uri
 $uri = parse_url($uri); // We parse that uri to path and query. Example: /products?id=5 , becomes array(2) { ["path"]=>string(8) "/products" ["query"]=>string(4) "id=5"}
