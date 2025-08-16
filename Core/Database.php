12f92324cfa59c2572db4b7f24a96bb76b33a1f4
Leximir <aleksa.stankovic@proton.me>
2025-08-16 19:30:00 +0200

    Tweaked Database class

--- Core/Database.php
+++ Core/Database.php
@@ -5,11 +5,11 @@ use PDO;
 class Database
 {
     public $connection;
+    public $statement;
 
     public function __construct($config, $username = 'root', $password = '')
     {
-        // Old $dsn :      $dsn="mysql:host=localhost;port=3306;dbname=my_app;charset=utf8mb4";
-        $dsn = ('mysql:' . http_build_query($config, '', ';')); // => host=localhost;port=3306;dbname=my_app;charset=utf8mb4
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
         $this->connection = new PDO($dsn, $username, $password, [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -18,8 +18,20 @@ class Database
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
-        $statement->execute($params);
-        return $statement;
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
     }
 }
