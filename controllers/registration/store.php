e458a60b19e6d8753c33cd5ddd5a673ce7613899
Leximir <aleksa.stankovic@proton.me>
2025-08-17 14:28:14 +0200

    Added php function to bycript the password

--- controllers/registration/store.php
+++ controllers/registration/store.php
@@ -39,7 +39,7 @@ if($user){
     // if not, save one to the database, and then log the user in, and redirect
     $db->query("INSERT INTO users(email, password) VALUES (:email, :password)",[
         'email' => $email,
-        'password' => $password
+        'password' => password_hash($password, PASSWORD_BCRYPT)
     ]);
     // Mark that tha user has logged in
     $_SESSION['logged_in'] = true;
