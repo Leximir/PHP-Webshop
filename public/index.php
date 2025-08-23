40e75d9d4387893a8981757d7968b4e25b43085c
Leximir <aleksa.stankovic@proton.me>
2025-08-23 22:18:06 +0200

    Installed composer and autoload package to clear a bit of our code

--- public/index.php
+++ public/index.php
@@ -3,15 +3,12 @@
 use Core\Session;
 
 const BASE_PATH = __DIR__ . '/../';
+
+require BASE_PATH . "vendor/autoload.php";
+
 session_start();
+
 require BASE_PATH . "Core/functions.php";
-
-spl_autoload_register(function ($class) {
-//  We added namespaces to a lot of classes and now we need to tweak this function to work properly
-    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class); // This: Core\Database becomses : Core/Database, and continues to work properly
-
-    require base_path("{$class}.php");
-});
 
 // Service Container
 require base_path('bootstrap.php');
