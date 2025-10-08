b70b3b7d7848ba08bfcd766fed5dfa41dfbb4786
Leximir <aleksa.stankovic@proton.me>
2025-10-08 13:08:32 +0200

    Connected to the correct database

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'my_app',
+        'dbname' => 'webshop',
         'charset' => 'utf8mb4'
     ]
 ];
