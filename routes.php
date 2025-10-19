fe153b07142231a6dd5ffba0fe02065c2fffa3d6
Leximir <aleksa.stankovic@proton.me>
2025-10-19 19:46:17 +0200

    Added all the routes related to products

--- routes.php
+++ routes.php
@@ -20,3 +20,14 @@ $router->post('/register', 'registration/store.php');
 $router->get('/login', 'session/create.php')->only('guest');
 $router->post('/session', 'session/store.php')->only('guest');
 $router->delete('/session', 'session/destroy.php')->only('auth');
+
+$router->get('/products', 'products/index.php');
+$router->get('/product', 'products/show.php');
+
+$router->get('/admin/products', 'admin/products/index.php')->only('auth');
+$router->get('/admin/product', 'admin/products/show.php')->only('auth');
+$router->delete('/admin/product', 'admin/products/destroy.php')->only('auth');
+$router->get('/admin/product/edit', 'admin/products/edit.php')->only('auth');
+$router->patch('/admin/product', 'admin/products/update.php')->only('auth');
+$router->get('/admin/products/create', 'admin/products/create.php')->only('auth');
+$router->post('/admin/products/create', 'admin/products/store.php')->only('auth');
